25cc65b4c2ead5a51d35de779baebf62f21569b6
Paolo Angeli <pangeli70@gmail.com>
2022-09-18 09:23:46 +0200

    Updated dependencies

--- deps.ts
+++ deps.ts
@@ -10,5 +10,5 @@ export * as  Drash from "https://deno.land/x/drash@v2.5.4/mod.ts";
 export { CORSService as DrashCorsService } from "https://deno.land/x/drash@v2.5.4/src/services/cors/cors.ts";
 
 // https://github
-export * as Edr from "https://raw.githubusercontent.com/Pangeli70/EDR/master/mod.ts";
-export * as Uts from "https://raw.githubusercontent.com/Pangeli70/UTS/master/mod.ts";
+export * as Edr from "https://raw.githubusercontent.com/Pangeli70/apg-edr/master/mod.ts";
+export * as Uts from "https://raw.githubusercontent.com/Pangeli70/apg-uts/master/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 /** -----------------------------------------------------------------------
- * @module [Tng]
+ * @module [Tng] SSR HTML template engine
  * @author [APG] ANGELI Paolo Giusto
  * ------------------------------------------------------------------------
 */
-export { ApgTngService } from "./src/classes/ApgTngService.ts"
+export { ApgTngService} from "./src/classes/ApgTngService.ts"
 export type { IApgTngServiceOptions } from "./src/classes/ApgTngService.ts"
