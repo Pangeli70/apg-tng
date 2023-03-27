9b147d63047fbd3df6c013791a87c2118e8d68c0
Paolo Angeli <pangeli70@gmail.com>
2023-03-27 03:03:27 +0200

    Deps updates

--- deps.ts
+++ deps.ts
@@ -4,18 +4,22 @@
  * ------------------------------------------------------------------------
 */
 // https://deno.land/std
-export * as StdFs from "https://deno.land/std@0.153.0/fs/mod.ts";
-export * as StdPath from "https://deno.land/std@0.153.0/path/mod.ts";
+export * as StdFs from "https://deno.land/std@0.180.0/fs/mod.ts";
+export * as StdPath from "https://deno.land/std@0.180.0/path/mod.ts";
 
 //https://deno.land/x/dotenv
 export * as DotEnv from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
 // https://deno.land/x/drash
-export * as  Drash from "https://deno.land/x/drash@v2.5.4/mod.ts";
+export * as  Drash from "https://deno.land/x/drash@v2.7.1/mod.ts";
 
 // https://deno.land/x/drash_middleware
-export { CORSService as DrashCorsService } from "https://deno.land/x/drash@v2.5.4/src/services/cors/cors.ts";
+export { CORSService as DrashCorsService } from "https://deno.land/x/drash@v2.7.1/src/services/cors/cors.ts";
 
 // https://github
-export * as Edr from "https://raw.githubusercontent.com/Pangeli70/apg-edr/master/mod.ts";
+
 export * as Uts from "https://raw.githubusercontent.com/Pangeli70/apg-uts/master/mod.ts";
+export * as Edr from "https://raw.githubusercontent.com/Pangeli70/apg-edr/master/mod.ts";
+
+// export * as Uts from "../apg-uts/mod.ts";
+// export * as Edr from "../apg-edr/mod.ts";
